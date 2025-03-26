fc9143225628683908ac3bce362360048581e216
mr-good-cat06 <tanvir8499@gmail.com>
2025-03-26 18:34:09 +0900

    Beautify

--- Terraform/modules/vpc/main.tf
+++ Terraform/modules/vpc/main.tf
@@ -5,10 +5,30 @@ resource "aws_vpc" "main" {
 }
 
 
-resource "aws_subnet" "private" {
+/*
+resource "aws_subnet" "private_subnet" {
     for_each = {for idx, cidr in var.private_subnet_cidr : idx => cidr}
     vpc_id = aws_vpc.main.id
     cidr_block = each.value
     availability_zone = var.availability_zone[each.key % length(var.availability_zone)]
+
+    tags = {
+    Name = var.subnet_names[each.key]
+  }
 }
+
+*/
+
+
+
+resource "aws_subnet" "private" {
+  count             = max(length(var.private_subnet_cidrs), length(var.availability_zones))
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index % length(var.availability_zones)]
+  tags = {
+    Name = var.subnet_names[each.key]
+  }
+}
+
 
