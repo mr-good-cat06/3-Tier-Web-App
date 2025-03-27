3693c16d905c037f278b2c1f4439a29fd4030513
mr-good-cat06 <tanvir8499@gmail.com>
2025-03-27 09:38:44 +0900

    VPC v2

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -17,5 +17,6 @@ module "vpc" {
     availability_zones = var.availability_zones
     private_subnet_cidrs = var.private_subnet_cidrs
     public_subnet_cidrs = var.public_subnet_cidrs
-    
+    subnet_names = var.subnet_names
+
 }
--- Terraform/modules/vpc/variables.tf
+++ Terraform/modules/vpc/variables.tf
@@ -1,16 +1,13 @@
 variable "vpc_cidr" {
     type = string
-    default = "110.10.10.0/24"
 }
 
 variable "availability_zones" {
     type = list(string)
-    default = [ "ap-northeast-1", "ap-northeast-2"]
 }
 
 variable "private_subnet_cidrs" {
     type = list(string)
-    default = [ "10.10.10.16/28", "10.10.10.32/28", "10.10.10.48/28", "10.10.10.64/28", "10.10.10.80/28", "10.10.10.96/28" ]
 }
 
 variable "public_subnet_cidrs" {
@@ -20,6 +17,5 @@ variable "public_subnet_cidrs" {
 
 variable "subnet_names" {
     type = list(string)
-    default = [ "web-1", "web-2", "app-1", "app-2", "db-1", "db-2" ]
 
 }
